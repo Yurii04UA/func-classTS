279d2ed45a23b808ef0c65c07e3e8f5c6450f661
Yurii04UA <uyriparaka@gmail.com>
2022-09-15 23:43:01 +0300

    class on TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require("path")
-const HTMLWebpackPlugin = require("html-webpack-plugin")
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
-const TerserPlugin = require("terser-webpack-plugin")
-const ESLintPlugin = require("eslint-webpack-plugin")
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
-const isDev = process.env.NODE_ENV === "development"
+const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
 
 const optimization = () => {
     const config = {
         splitChunks: {
-            chunks: "all",
+            chunks: 'all',
         },
         minimize: false,
         minimizer: [],
@@ -28,32 +28,37 @@ const filename = (extensions) =>
     isDev ? `[name].${extensions}` : `[name].[hash].${extensions}`
 
 module.exports = {
-    mode: "development",
-    context: path.resolve(__dirname, "src"),
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
     entry: {
-        main: "./script/index.ts",
+        main: './script/index.ts',
     },
     output: {
-        filename: filename("js"),
-        path: path.resolve(__dirname, "dist"),
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
     resolve: {
-        extensions: [".ts", ".js", ".json"],
+        extensions: ['.ts', '.js', '.json'],
     },
     devServer: {
         port: 8080,
-        watchFiles: ["src/index.html"],
+        watchFiles: ['src/index.html'],
+        client: {
+            overlay: {
+                errors: true,
+                warnings: false,
+            },
+        },
     },
-    devtool: isDev ? "source-map" : "inline-source-map",
+    devtool: isDev ? 'source-map' : 'inline-source-map',
     plugins: [
         new HTMLWebpackPlugin({
-            template: "./index.html",
+            template: './index.html',
         }),
 
         new ESLintPlugin({
-            extensions: ["ts"],
-            fix: true,
+            extensions: ['ts'],
         }),
     ],
     optimization: optimization(),
@@ -61,15 +66,15 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                loader: "ts-loader",
+                loader: 'ts-loader',
             },
             {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader",
+                    loader: 'babel-loader',
                     options: {
-                        presets: ["@babel/preset-env"],
+                        presets: ['@babel/preset-env'],
                     },
                 },
             },
